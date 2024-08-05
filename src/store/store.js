89f0d0656792b58393feb367c2e4527b99cb827b
MUTABAZI Dieudonne <mudonne18@gmail.com>
2024-08-05 20:31:15 +0200

    removed some workouts related files and codes

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import sampleReducer from './sampleFeature/slice';
 import authSliceReducer from './authFeature/authSlice';
 import usersSliceReducer from './usersFeature/usersSlice';
-import workoutsSliceReducer from './workoutsFeature/workoutsSlice';
 
 export const store = configureStore({
   reducer: {
     sampleSlice: sampleReducer,
     authReducer: authSliceReducer,
     usersReducer: usersSliceReducer,
-    workoutsReducer: workoutsSliceReducer,
   },
 });
